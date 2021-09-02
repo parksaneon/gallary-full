9c711d77f9d2722533cf659089f947dfea22643b
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-02 21:21:04 +0900

    upload imge function with useState

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("이미지 파일을 업로드 해주세요.");
   return (
     <form>
-      <label htmlFor="image">사진</label>
-      <input id="image" type="file" />
+      <label htmlFor="image">{fileName}</label>
+      <input
+        id="image"
+        type="file"
+        onChange={(e) => {
+          const imageFile = e.target.files[0];
+          setFile(imageFile);
+          setFileName(imageFile.name);
+        }}
+      />
       <button type="submit">제출</button>
     </form>
   );
