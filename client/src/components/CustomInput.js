7cca35b3df02c388d98baa1183104d2e8527c13e
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-06 22:27:35 +0900

    feat : register in form Component

---
+++ client/src/components/CustomInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CustomInput = ({ label, value, setValue, type = "text" }) => {
+  <div>
+    <label>{label}</label>
+    <input
+      style={{ width: "100%" }}
+      value={value}
+      type={type}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  </div>;
+};
+
+export default CustomInput;
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import CustomInput from "../components/CustomInput";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const RegisterPage = () => {
-  return <h3>Register</h3>;
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
+  const submitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      if (username.length < 3) throw new Error("회원ID는 3자 이상입니다.");
+      if (password.length < 6) throw new Error("비밀번호는 6자 이상입니다.");
+      if (passwordCheck !== password)
+        throw new Error("비밀번호가 일치하지 않습니다.");
+      const result = await axios.post("/users/register", {
+        name,
+        username,
+        password,
+      });
+      toast.success("회원가입 성공");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        marginTop: 100,
+        maxWidth: 350,
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <h3>회원가입</h3>
+      <form onSubmit={submitHandler}>
+        <CustomInput label="이름" value={name} setValue={setName} />
+        <CustomInput label="회원ID" value={username} setValue={setUserName} />
+        <CustomInput
+          label="비밀번호"
+          value={password}
+          setValue={setPassword}
+          type="password"
+        />
+        <CustomInput
+          label="비밀번호 확인"
+          value={passwordCheck}
+          setValue={setPasswordCheck}
+          type="password"
+        />
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
