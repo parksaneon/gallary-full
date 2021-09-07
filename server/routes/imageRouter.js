cfa7c8df4839e8c39dec10e783d357be23f8a85c
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-07 22:16:48 +0900

    feat : like and unlike

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -55,13 +55,41 @@ imageRouter.delete("/:imageId", async (req, res) => {
   }
 });
 
-imageRouter.patch("/:imageId/like", (req, res) => {
+imageRouter.patch("/:imageId/like", async (req, res) => {
   // 유저 권한 확인
   // like 중복 안되도록 확인
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 이미지 아이디 입니다.");
+    const image = await Image.findByIdAndUpdate(
+      { _id: req.params.imageId },
+      { $addToSet: { likes: req.user.id } },
+      { new: true }
+    );
+    res.json(image);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 });
-imageRouter.patch("/:imageId/unlike", (req, res) => {
+imageRouter.patch("/:imageId/unlike", async (req, res) => {
   // 유저 권한 확인
   // like 중복 취소 안되도록 확인
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 이미지 아이디 입니다.");
+    const image = await Image.findByIdAndUpdate(
+      { _id: req.params.imageId },
+      { $pull: { likes: req.user.id } },
+      { new: true }
+    );
+    res.json(image);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = { imageRouter };
