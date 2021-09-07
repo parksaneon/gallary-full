bd2272d598724966a2a0a1356a6469564bb6b806
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-07 21:59:47 +0900

    feat : delete image

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -2,6 +2,11 @@ const { Router } = require("express");
 const imageRouter = Router();
 const Image = require("../models/Image");
 const { upload } = require("../middleware/imageUpload");
+const fs = require("fs");
+const { promisify } = require("util");
+const mongoose = require("mongoose");
+
+const fileUnlink = promisify(fs.unlink);
 
 // upload.single("image") 라는 미들웨어를 사용함으로서 req에서 데이터에 접근이 가능
 imageRouter.post("/images", upload.single("image"), async (req, res) => {
@@ -27,13 +32,27 @@ imageRouter.post("/images", upload.single("image"), async (req, res) => {
 
 imageRouter.get("/", async (req, res) => {
   // public 이미지들만 제공
-  const images = await Image.find();
+  const images = await Image.find({ public: true });
   res.json(images);
 });
 
-imageRouter.delete("/:imageId", (req, res) => {
+imageRouter.delete("/:imageId", async (req, res) => {
   // 유저 권한 확인
   // 사진 삭제
+  // 1. uploads 폴더의 사진 데이터 삭제
+  // 2. DB의 image 문서 삭제
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 이미지 아이디 입니다.");
+    const image = await Image.findOneAndDelete({ _id: req.params.imageId });
+    if (!image) return res.json({ message: "요청하신 이미지가 없습니다." });
+    await fileUnlink(`./uploads/${image.key}`);
+    res.json({ message: "요청하신 이미지가 삭제됐습니다.", image });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 imageRouter.patch("/:imageId/like", (req, res) => {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
 const { hash, compare } = require("bcryptjs");
-const mongoose = require("mongoose");
+const Image = require("../models/Image");
 
 userRouter.post("/register", async (req, res) => {
   try {
@@ -76,6 +76,16 @@ userRouter.get("/me", (req, res) => {
   }
 });
 
-userRouter.get("/me", (req, res) => {});
+userRouter.get("/me/images", async (req, res) => {
+  // 본인의 사진들만 반환 ( public === false )
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    const images = await Image.find({ "user._id": req.user.id });
+    res.json(images);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = { userRouter };
