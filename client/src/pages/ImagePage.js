fcf5a40df86b06fc93c7b9673b08a57c49ce9d41
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-08 23:41:24 +0900

    feat : delete image

--- client/src/pages/ImagePage.js
+++ client/src/pages/ImagePage.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import { ImageContext } from "../context/ImageContext";
 import { AuthContext } from "./../context/AuthContext";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const ImagePage = () => {
+  const history = useHistory();
   const { imageId } = useParams();
   const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
   const [me] = useContext(AuthContext);
@@ -37,6 +39,22 @@ const ImagePage = () => {
     setHashLinked(!hasLinked);
   };
 
+  const deleteImage = (images) =>
+    images.filter((image) => image._id !== imageId);
+
+  const deleteHandler = async () => {
+    try {
+      if (!window.confirm("정말 해당 이미지를 삭제하시겠습니까?")) return;
+      const result = await axios.delete(`/images/${imageId}`);
+      toast.success(result.data.message);
+      setImages(images.filter((image) => image._id !== imageId));
+      setMyImages(myImages.filter((image) => image._id !== imageId));
+      history.push("/");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div>
       <h3>Image Page - {imageId}</h3>
@@ -46,6 +64,14 @@ const ImagePage = () => {
         src={`http://localhost:5000/uploads${image.key}`}
       />
       <span>좋아요 {image.likes.length}</span>
+      {me && image.user._id === me.userId && (
+        <button
+          style={{ float: "right", marginRight: 10 }}
+          onClick={deleteHandler}
+        >
+          삭제
+        </button>
+      )}
       <button style={{ float: "right" }} onClick={onSubmit}>
         {hasLinked ? "좋아요 취소" : "좋아요"}
       </button>
