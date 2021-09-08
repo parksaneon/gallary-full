f98352ae9fc60d2c99b4d4b1adafa8239440bb18
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-08 22:22:12 +0900

    apply display flex to ImageList component

--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { ImageContext } from "../context/ImageContext";
+import "./ImageList.css";
 
 const ImageList = () => {
   const { images, myImages, isPublic, setIsPublic } = useContext(ImageContext);
-
+  const [me] = useContext(AuthContext);
   const imgList = (isPublic ? images : myImages).map((image) => (
     <img
       key={image.key}
@@ -18,10 +20,12 @@ const ImageList = () => {
       <h3 style={{ display: "inline-block", marginRight: 10 }}>
         Image List({isPublic ? "공개" : "개인"}공개 사진)
       </h3>
-      <button onClick={() => setIsPublic(!isPublic)}>
-        {isPublic ? "개인" : "공개"} + 사진 보기
-      </button>
-      {imgList}
+      {me && (
+        <button onClick={() => setIsPublic(!isPublic)}>
+          {isPublic ? "개인" : "공개"} + 사진 보기
+        </button>
+      )}
+      <div className="image-list-container"> {imgList}</div>
     </div>
   );
 };
