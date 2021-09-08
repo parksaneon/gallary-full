baa770115380db4e6c18119013f14f14a238dab2
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-09-08 23:24:41 +0900

    feat : like or unliket

--- client/src/pages/ImagePage.js
+++ client/src/pages/ImagePage.js
@@ -1,19 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { ImageContext } from "../context/ImageContext";
+import { AuthContext } from "./../context/AuthContext";
+import axios from "axios";
 
 const ImagePage = () => {
   const { imageId } = useParams();
-  const { images, myImages } = useContext(ImageContext);
+  const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
+  const [me] = useContext(AuthContext);
+  const [hasLinked, setHashLinked] = useState(false);
 
   const image =
     images.find((image) => image._id === imageId) ||
     myImages.find((image) => image._id === imageId);
 
+  useEffect(() => {
+    if (me && image && image.likes.includes(me.userId)) setHashLinked(true);
+  }, [me, image]);
+
+  if (!image) return <h3>Loading...</h3>;
+
+  const updateImage = (images, image) =>
+    [...images.filter((image) => image._id !== imageId)].sort(
+      (a, b) =>
+        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    );
+
+  const onSubmit = async () => {
+    const result = await axios.patch(
+      `/images/${imageId}/${hasLinked ? "unlike" : "like"}`
+    );
+
+    if (result.data.public) setImages(updateImage(images, result.data));
+    else setMyImages(updateImage(myImages, result.data));
+
+    setHashLinked(!hasLinked);
+  };
+
   return (
     <div>
       <h3>Image Page - {imageId}</h3>
-      <img alt={imageId} src={`http://localhost:5000/uploads${image.key}`} />
+      <img
+        style={{ width: "100%" }}
+        alt={imageId}
+        src={`http://localhost:5000/uploads${image.key}`}
+      />
+      <span>좋아요 {image.likes.length}</span>
+      <button style={{ float: "right" }} onClick={onSubmit}>
+        {hasLinked ? "좋아요 취소" : "좋아요"}
+      </button>
     </div>
   );
 };
